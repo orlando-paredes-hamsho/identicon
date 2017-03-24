e3905fa295c65d714bc5ae6c5570be71a721a2af
Orlando Paredes Hamsho <paredesorlandoh@gmail.com>
2017-03-24 01:21:15 +0000

    patch: add hash_input and Add image.ex

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,10 +1,15 @@
 defmodule Identicon do
+
   def main(input) do 
     input
     |> hash_input
   end
   
   def hash_input(input) do 
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
     
+    %Identicon.Image{hex: hex}
   end
+  
 end
